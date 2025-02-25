ebc79259b7aaf74b06f87584af8611f81f1c9389
Gaurav Patel <gaurav.gkp29@gmail.com>
2025-02-25 12:04:51 +0530

    Task 1 - Simple number or Empty String

--- src/views/App.js
+++ src/views/App.js
@@ -1,35 +1,41 @@
 import React, {Component} from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 
-class App extends Component {
+class StringCalculator extends Component {
     constructor(props){
-      super(props)
-      this.state = {
+        super(props)
+        this.state = {
+            showResult:false,
+            sumValue: '',
+            textValue: ''
+        }
+    }
 
-      }
+    add(){
+        let sumValue = ''
+        let textValue = this.state.textValue
+        if(!textValue){
+            sumValue = 0
+        }
+        let delimiter = /,/;
+        sumValue = textValue.split(delimiter).map(Number).reduce((sum, num) => sum + num, 0);
+        this.setState({sumValue, showResult: true})    
     }
 
     render() {
       return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+            <header className="App-header">
+                <input type="text" placeHolder="Type your string.." value={this.state.textValue} onChange={(e)=> this.setState({textValue: e.target.value})} />
+                <button type="submit" onClick={this.add.bind(this)}>Submit</button>
+                {
+                    this.state.showResult ? 
+                    <p>Sum is: {this.state.sumValue}</p> : null
+                }
+            </header>
         </div>
       );
     }
 }
 
-export default App;
+export default StringCalculator;
