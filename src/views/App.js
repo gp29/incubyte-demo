90f9af8f2cf59053efdd4e21aff14d3730c63a37
Gaurav Patel <gaurav.gkp29@gmail.com>
2025-02-25 17:23:43 +0530

    Task 10, 11, 12 : All Task Done

--- src/views/App.js
+++ src/views/App.js
@@ -5,39 +5,60 @@ class StringCalculator extends Component {
     constructor(props){
         super(props)
         this.state = {
-            showResult:false,
-            sumValue: '',
-            textValue: ''
+            sumArr: []
         }
     }
 
-    add(){
+    componentDidMount(){
+        this.add("")
+        this.add("1")
+        this.add("1,2")
+        this.add("1,2,3,4,5")
+        this.add("1\n2,3")
+        this.add("//;\n1;2")
+        this.add("1\n2,-3,-4")
+        this.add("1\n2,1001,1002")
+        this.add("//[***]\n1***2***3")
+        this.add("//[*][%]\n1*2%3")
+        this.add("//[**][%%]\n1**2%%3")
+    }
+
+    add(input){
+        let sumArr = this.state.sumArr
         let sumValue = 0
-        let textValue = this.state.textValue
-        if(!textValue){
-            sumValue = 0
+        let textValue = input
+        if(textValue){
+            let delimiter = /,|\n|;/;
+            const match = textValue.match(/^\/\/(\[.*\])[\n](.*)/);
+            if(match){
+                textValue = match[2];
+
+                const delimiterVal = match[1];
+                const delimiters = delimiterVal.match(/\[(.*?)\]/g).map(d => d.slice(1, -1));
+                delimiter = new RegExp(delimiters.map(d => d.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')).join('|'));
+            }
+            let numArray = textValue.split(delimiter).map(num => parseFloat(num) || 0);
+            const negatives = numArray.filter(num => num < 0);
+            if (negatives.length) {
+                sumValue = `Negative numbers not allowed: ${negatives.join(", ")}`
+            }
+            else{
+                numArray = numArray.filter((x) => x < 1000)
+                sumValue = numArray.reduce((sum, num) => sum + num, 0);
+            }
         }
-        let delimiter = /[/n,;]+/
-        let numArray = textValue.split(delimiter).map(num => parseFloat(num) || 0);
-        const negatives = numArray.filter(num => num < 0);
-        if (negatives.length) {
-            alert(`Negative numbers not allowed: ${negatives.join(", ")}`);
-            return;
-        }
-        numArray = numArray.filter((x) => x < 1000)
-        sumValue = numArray.reduce((sum, num) => sum + num, 0);
-        this.setState({sumValue, showResult: true})    
+        sumArr.push({input, sum: sumValue})
+        this.setState({sumArr})    
     }
 
     render() {
       return (
         <div className="App">
             <header className="App-header">
-                <input type="text" placeholder="Type your string.." value={this.state.textValue} onChange={(e)=> this.setState({textValue: e.target.value})} />
-                <button type="submit" onClick={this.add.bind(this)}>Submit</button>
                 {
-                    this.state.showResult ? 
-                    <p>Sum is: {this.state.sumValue}</p> : null
+                    this.state.sumArr.map((e, key) => {
+                        return <p key={key}>Result of string "{e.input}" is: {e.sum}</p> ;
+                    })
                 }
             </header>
         </div>
